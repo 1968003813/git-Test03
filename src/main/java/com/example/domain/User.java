121dc97ecfb20b6aab24d6dccb7a6454d92610d7
1968003813 <1968003813@qq.com>
2018-03-09 21:23:30 +0800

    Initial commit

---
+++ src/main/java/com/example/domain/User.java
@@ -0,0 +1,20 @@
+package com.example.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User(Integer id) {
+
+        this.id = id;
+    }
+}
