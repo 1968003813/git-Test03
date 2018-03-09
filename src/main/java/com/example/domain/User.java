316533fa06355d7d4124aa5b84adf4319424d9a6
1968003813 <1968003813@qq.com>
2018-03-09 21:57:50 +0800

    My First Branch

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -4,6 +4,21 @@ import java.io.Serializable;
 
 public class User implements Serializable{
     private Integer id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(Integer id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
 
     public Integer getId() {
         return id;
